0687795b28c6527874d2472dbab493e151edf1c6
Eric Feirouz <efeirouz@u.rochester.edu>
2019-03-27 09:52:28 -0400

    index

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home | Midterm Project</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans|IBM+Plex+Serif" rel="stylesheet">
+</head>
+<body class="grid-layout">
+	<div class="head">
+		<?php include "inc/logo.inc"; ?>
+	</div>
+
+	<div class="nav-bar">
+		<?php include "inc/nav.inc"; ?>
+	</div>
+
+	<div class="weak">
+		<aside class="quote">
+			<p>Heroes Never Die!</p>
+			<p>-Mercy</p>
+		</aside>
+	</div>
+
+	<div class="main">
+		<ol class="boxes">
+			<li><a href="info.php">Learn More!</a></li>
+		</ol>
+	</div>
+<?php include "inc/scripts.inc"; ?>
