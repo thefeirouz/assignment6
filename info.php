0e761e2c08e955d5a51778c041a1c783e1c0333a
Eric Feirouz <efeirouz@u.rochester.edu>
2019-03-27 09:48:39 -0400

    Pushing items

---
+++ info.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Overwatch Information | Midterm Project</title>
+	<link rel="stylesheet" href="css/styles-two.css">
+	<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans|IBM+Plex+Serif" rel="stylesheet">
+</head>
+
+<body class="grid-container">
+	<div class="head">
+		<?php include "inc/logo.inc"; ?>
+	</div>
+
+	<div class="nav-bar">
+		<?php include "inc/nav.inc"; ?>
+	</div>
+
+
+	<div class="overview">
+		<article class="flex-container">
+			<div class="down">
+				<h2>Overview</h2>
+			</div>
+			<div class="information">
+				<p>Overwatch is a team-based multiplayer first-person shooter developed and published by Blizzard Entertainment and released on May 24, 2016 for PlayStation 4, Xbox One, and Windows. Described as a "hero shooter", Overwatch assigns players into two teams of six, with each player selecting from a roster of 30 characters, known as "heroes", each with a unique style of play whose roles are divided into three general categories that fit their role. Players on a team work together to secure and defend control points on a map or escort a payload across the map in a limited amount of time. Players gain cosmetic rewards that do not affect gameplay, such as character skins and victory poses, as they play the game. The game was initially launched with casual play, with a competitive ranked mode, various 'arcade' game modes, and a player-customizable server browser subsequently included following its release. Additionally, Blizzard has added new characters, maps, and game modes post-release, all free of charge, with the only additional cost to players being optional loot boxes to earn cosmetic items.</p>
+			</div>
+			<div class="picture-two">
+				<figure>
+					<img src="images/blizzard.png" alt="Blizzard">
+				</figure>
+			</div>
+		</article>
+	</div>
+
+	<div class="gameplay">
+		<article class="flex-container">
+			<div class="down-two">
+				<h2>Gameplay</h2>
+			</div>
+			<div class="information-three">
+				<p>Overwatch features a number of different game modes, principally designed around squad-based combat with two opposing teams of six players each. Players select one of over two dozen pre-made hero characters from one of three class types: Damage heroes that deal most of the damage to attack or defend control points, Tank heroes that can absorb a large amount of damage, and Support heroes that provide healing or other buffs for their teammates. Each hero has a unique skill kit, defining their intrinsic attributes like health points and running speed, their primary attacks, several active and passive skills, and an ultimate ability that can only be used after it has been charged through dealing damage. Players can change their hero during the course of a match, as a goal of Overwatch's design was to encourage dynamic team compositions that adapt to the situation. The game's genre has been described by some journalists as a "hero shooter", due to its design around specific heroes and classes.</p>
+			</div>
+			<div>
+				<figure class="picture">
+					<img src="images/grandmaster.png" alt="Grandmaster Icon">
+				</figure>
+			</div>
+		</article>
+	</div>
+
+	<div class="plot">
+		<article class="flex-container">
+			<div class="down-two">
+				<h2>Plot</h2>
+			</div>
+			<div class="information-two">
+				<p>The backstory to Overwatch is described through animated shorts and other information distributed by Blizzard in promoting the game. Overwatch is set sixty years into the future of a fictionalized Earth, thirty years after the resolution of what is known as the "Omnic Crisis". Prior to the Omnic Crisis, humanity had been in a golden age of prosperity and technology development. Humans developed robots with artificial intelligence called "Omnics", which were put to use to achieve economic equality, and began to be treated as people in their own right. The Omnic Crisis began when the worldwide automated "omnium" facilities that produced them, started producing a series of lethal, hostile robots, that attacked humankind. The United Nations quickly formed Overwatch, an international task force to combat this threat and restore order.</p>
+				<footer>(Source: <a href="https://en.wikipedia.org/wiki/Overwatch_(video_game)" target="blank"> Overwatch Wikipedia Page)</a></footer>
+			</div>
+			<div>
+				<figure class="picture-three">
+					<img src="images/heroes.jpg" alt="Heroes">
+				</figure>
+			</div>
+		</article>
+	</div>
+
+	<div class="sign-up">
+		<form>
+			<fieldset>
+				<legend>Sign up for our Overwatch Newsletter!</legend>
+				Email Address:<br>
+				<input type="text" name="email"><br>
+				<input type="submit" value="Submit">
+			</fieldset>
+		</form>
+	</div>
+		
+<?php include "inc/scripts.inc"; ?>
